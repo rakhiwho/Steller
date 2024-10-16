d38e6584d77048a9f2c80d0609f6efa79ce328eb
rakhiwho <rakhydubey04@gamil.com>
2024-10-16 16:44:43 +0530

    changed vite.comfig.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/'
+  build: {
+    outDir: 'dist' // This ensures that the build output goes to the 'dist' directory
+  }
 })
