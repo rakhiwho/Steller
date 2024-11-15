9005f97dc74d77cff0a5b73ec7e1c12679f9d4df
Rakhi Dubey <149473696+rakhiwho@users.noreply.github.com>
2024-11-15 21:44:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(cookieParser())
 
  
 
-mongoose.connect(process.env.MONGO_DB_URL)
+mongoose.connect(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch((err) => console.error('Error connecting to MongoDB:', err));
 
