dc208d9701cf9b3e19fddf329da4726c4f23895c
Rakhi Dubey <149473696+rakhiwho@users.noreply.github.com>
2024-11-18 17:21:09 +0530

    added logout route

--- backend/src/api/user.js
+++ backend/src/api/user.js
@@ -23,7 +23,7 @@ router.get('/get/:id', verifyToken , getFunction)
 router.get('/userProfile' , verifyToken , get_loggedIn_User);
 router.get('/getUsers', verifyToken , friends )
 router.get('/userInfo/:id', verifyToken , userInfo )
-
+router.post('/logout' , verifyToken , logout);
 // updating 
 router.put('/editProfile' , verifyToken , upload.single('profilePic') , editInfo )
 router.put('/add-story' , verifyToken , Uplaod_Story);
